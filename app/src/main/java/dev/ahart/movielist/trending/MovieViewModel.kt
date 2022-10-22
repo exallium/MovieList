ef5d995725e9ac4d119e65f3b571aecea1e79806
Alex Hart <alex.hart658@gmail.com>
2022-10-22 13:23:42 -0300

    Add view model wiring.

--- app/src/main/java/dev/ahart/movielist/trending/MovieViewModel.kt
+++ app/src/main/java/dev/ahart/movielist/trending/MovieViewModel.kt
@@ -4,8 +4,31 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.setValue
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import dev.ahart.movielist.trending.retrofit.MovieRetrofitService
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.withContext
 
-class MovieViewModel : ViewModel() {
+class MovieViewModel(
+  private val movieRetrofitService: MovieRetrofitService = MovieRetrofitService.createService(),
+  private val ioDispatcher: CoroutineDispatcher = Dispatchers.IO
+) : ViewModel() {
   var uiState by mutableStateOf<List<Movie>>(emptyList())
     private set
+
+  init {
+    viewModelScope.launch {
+      uiState = withContext(ioDispatcher) {
+        val trending = movieRetrofitService.getTrending()
+        trending.results.map {
+          Movie(
+            uri = it.getFullPosterPath(),
+            name = listOfNotNull(it.title, it.name).firstOrNull() ?: ""
+          )
+        }
+      }
+    }
+  }
 }
